e1a189bdc52414768585636d76633ad54b823088
Elena Lopez <emloppal@gmail.com>
2025-06-29 21:50:28 +0100

    fix: a bit of styling to error pages

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -17,9 +17,16 @@ export default function ErrorPage({
   }, [error]);
   return (
     <>
-      <h1>Oh no! Something went wrong</h1>
-      <Link href={"/"}>Go home</Link>
-      <button onClick={() => reset()}>Try again </button>
+      <section className="mx-5 md:mx-16 flex flex-col items-center justify-center">
+        <h1>Oh no! Something went wrong</h1>
+        <Link
+          href={"/"}
+          className="md:w-fit justify-between md:text-lg bg-white rounded-4xl text-background flex items-center gap-3 font-semibold ps-5 pe-3 py-2 hover:bg-neutral-700 hover:text-white"
+        >
+          Go home
+        </Link>
+        <button onClick={() => reset()}>Try again </button>
+      </section>
     </>
   );
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,9 +5,16 @@ import Link from "next/link";
 //set-up done needs styling/copy
 export default function NotFound() {
   return (
-    <>
-      <h1>Whatever you are looking for does not exist</h1>
-      <Link href={"/"}>Go home</Link>
-    </>
+    <section className="mx-5 md:mx-16 flex flex-col items-center justify-center ">
+      <h1 className="text-3xl text-center md:text-6xl font-bold md:mb-4 text-green-accent lg:max-w-[40dvw] py-6 md:py-8">
+        Whatever you are looking for does not exist.
+      </h1>
+      <Link
+        href={"/"}
+        className="md:w-fit justify-between md:text-lg bg-white rounded-4xl text-background flex items-center gap-3 font-semibold px-5 py-2 hover:bg-neutral-700 hover:text-white"
+      >
+        Go home
+      </Link>
+    </section>
   );
 }
